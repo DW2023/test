6e319b2f5b0d1141e7cffd8438122767a76620c4
DW2023 <dwkim126@gmail.com>
2023-02-22 09:19:18 +0900

    Hello Thee 로 변경

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello worldgit status!");
+        System.out.println("Hello here!");
     }
-}
 
